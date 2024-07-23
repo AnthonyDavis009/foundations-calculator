cde67702abc1a36caea472e6064033ad0d215a10
Anthony Davis <anthonybdavis.inbox@gmail.com>
2024-07-23 18:31:38 -0400

    Revamped functionality to use one operator at a time per calculation

--- script.js
+++ script.js
@@ -1,142 +1,134 @@
-
 const clearBtn = document.querySelector("#clr-btn");
 const calcInput = document.querySelector("#calc-input");
 const calcBtnUsable = document.querySelectorAll(".calc-btn-usable");
 const enterBtn = document.querySelector("#enter-btn");
 
 calcBtnUsable.forEach(node => {
-    node.addEventListener("click", () => {
+    node.addEventListener("click", e => {
     eraseErrMsg();
-    if(inputMisuseCase() == "leading operator") {
-        calcInput.value = "";
-     } else if(inputMisuseCase() == "adjacent operator") {
-         calcInput.value = calcInput.value.slice(0, calcInput.value.length - 1);
-     }
-     calcInput.value += node.textContent;
+    if(isEventTargetErrCase(e.target.textContent)) {
+
+    } else {
+      calcInput.value += node.textContent;
+      calcInput.scrollLeft += 100;  
+
+      const actualWidth = calcInput.clientWidth;
+      calcInput.scrollLeft = calcInput.scrollWidth - actualWidth;
+    }
     });
 });
 
 calcInput.addEventListener("keydown", e => {
   eraseErrMsg();
-
   // Remember that strings are immutable and cannot edit them directly with array index [] 
   // But can instead copy/display their values
   let allowedInputArr = ['0','1','2','3','4','5','6','7','8','9','*','/','+','-', 'Backspace'];
+
   if(!allowedInputArr.includes(e.key)) {
     e.preventDefault();
-  } else if(e.key == "*") {
-    e.preventDefault();
-    calcInput.value += "×";
-  } else if(e.key == "/") {
-    e.preventDefault();
-    calcInput.value += "÷";
-  } else if(e.key == "-") {
-    e.preventDefault();
-    calcInput.value += "–";
-  }
+  } else {
+
+    let operatorIfUsed = e.key;
+
+    if(e.key == "*") operatorIfUsed = "×";
+    else if (e.key == "/") operatorIfUsed = "÷";
+    else if (e.key == "-") operatorIfUsed = "–";
   
-  if(inputMisuseCase() == "leading operator") {
-     calcInput.value = "";
-  } else if(inputMisuseCase() == "adjacent operator") {
-      calcInput.value = calcInput.value.slice(0, calcInput.value.length - 1);
+    if(isEventTargetErrCase(operatorIfUsed)) {
+        e.preventDefault();
+    } else {
+      if(operatorIfUsed.length > 0 && operatorIfUsed != 'Backspace') {
+        e.preventDefault();
+        calcInput.value += operatorIfUsed;
+      } 
+      const actualWidth = calcInput.clientWidth;
+      calcInput.scrollLeft = calcInput.scrollWidth - actualWidth;
+    }
   }
 });
 
 enterBtn.addEventListener("click", () => {
-  let calcInputCurr = calcInput.value;
-  let recombinedArr = recombineArrays(obtainNumsUsed(calcInputCurr), obtainArithOperatorsUsed(calcInputCurr));
-  calcInput.value = getCalculations(recombinedArr);
+  calcInput.value = operation();
+  calcInput.scrollLeft = 0;  
 });
 
 clearBtn.addEventListener("click", () => calcInput.value = "");
-
-function inputMisuseCase() {
-  const arithOperatorsValid = ["×", "÷", "+", "–"]
-  if(arithOperatorsValid.includes(calcInput.value[0])) {
-    return "leading operator";
-  } else if(
-    arithOperatorsValid.includes(calcInput.value[calcInput.value.length - 1]) &&
-    arithOperatorsValid.includes(calcInput.value[calcInput.value.length - 2])) {
-      return "adjacent operator";
-  }
-}
-
-function obtainNumsUsed(calcInputCurr) {
-    // replaceAll is not destructive so assignment must be used to modify numsUsed
-      return calcInputCurr
-      .replaceAll("×", ",")
-      .replaceAll("÷", ",")
-      .replaceAll("+", ",")
-      .replaceAll("–", ",")
-      .split(",")
-      .map(num => Number(num));
-  }
-  
-function obtainArithOperatorsUsed(calcInputCurr) {
-    let arithOperationsArr = [];
-    for(const char of calcInputCurr) {
-        if(char == "×" || char == "÷" ||
-           char == "+" || char == "–") {
-           arithOperationsArr.push(char);
-        }
-    }
-    return arithOperationsArr;
-}
-
-function recombineArrays(numsUsed, arithOperators) {
-    let numsUsedArr = numsUsed;
-    let arithOperationsArr = arithOperators;
-    let combinedArr = [];
-    for(let i = 0; i < numsUsedArr.length; i++) {
-      combinedArr.push(numsUsedArr[i]);
-      if(i < numsUsedArr.length - 1) {
-        combinedArr.push(arithOperationsArr[i]);
-      }
-    }
-    return combinedArr;
-}
-
-function getCalculations(recombinedArr) {
-    let calcStringArr = recombinedArr;
-    // Multiplication and division
-    for(let i = 1; i < calcStringArr.length; i+=2) {
-      if(calcStringArr[i] == "×") {
-        calcStringArr.splice(i - 1, 3, multiply(calcStringArr, i));
-        i -= 2;
-      } else if(calcStringArr[i] == "÷") {
-        if(calcStringArr[i - 1] == 0 && calcStringArr[i + 1] == 0) {
-          return "Err: ÷ by 0";
-        } else {
-          calcStringArr.splice(i - 1, 3, divide(calcStringArr, i));
-          i -= 2;
-        }
-      }
-    }
-    // Addition and subtraction
-    for(let i = 1; i < calcStringArr.length; i+=2) {
-      if(calcStringArr[i] == "+") {
-        calcStringArr.splice(i - 1, 3, add(calcStringArr, i));
-        i -= 2;
-      } else if(calcStringArr[i] == "–") {
-        calcStringArr.splice(i - 1, 3, subtract(calcStringArr, i));
-        i -= 2;
-      }
-    }
-
-    if(Number.isInteger(calcStringArr[0])) {
-      return calcStringArr[0];
-    } else {
-      return calcStringArr[0].toFixed(3);
-    }
-  }
-// Use const incase the function is ever attempted for some reason to be reassigned 
-const multiply = (calcStringArr, i) => calcStringArr[i-1] * calcStringArr[i+1];
-const divide = (calcStringArr, i) => calcStringArr[i-1] / calcStringArr[i+1];
-const add = (calcStringArr, i) => calcStringArr[i-1] + calcStringArr[i+1];
-const subtract = (calcStringArr, i) => calcStringArr[i-1] - calcStringArr[i+1];
 
 function eraseErrMsg() {
   if(calcInput.value == "Err: ÷ by 0") calcInput.value = "";
 }
 
+function isEventTargetErrCase(charRetrieved) {
+  if(
+  // If charRetrieved is operator and input is empty, set true to prevent leading operator
+    pressedArithOperator(charRetrieved) && 
+    calcInput.value == "") {
+    return true;
+  } else if(
+  // If charRetrieved is operator and input is empty, set true to prevent trailing operator
+    pressedArithOperator(charRetrieved) && 
+    pressedArithOperator(calcInput.value[calcInput.value.length - 1])) {
+    return true;
+  } else if(
+  // If charRetrieved is operator and input contains operator before, set true to prevent duplicate adjacent operators
+    attemptedExtraArithOperator(calcInput.value) &&
+    pressedArithOperator(charRetrieved)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function pressedArithOperator(character) {
+  const arithOperatorsValid = ["×", "÷", "+", "–"]
+  return arithOperatorsValid.includes(character) ? true : false;
+}
+
+function attemptedExtraArithOperator(calcInputStr) {
+  if(
+  calcInputStr.includes("×") ||
+  calcInputStr.includes("÷") ||
+  calcInputStr.includes("+") ||
+  calcInputStr.includes("–")) {
+    return true;
+  }
+}
+
+function operation() {
+  let numPairArr = extractInputNumPair();
+  let operatorUsed = extractUsedOperator();
+
+  if(numPairArr[0] == 0 && numPairArr[1] == 0 && operatorUsed == "÷") return "Err: ÷ by 0";
+
+  if(operatorUsed == "×") return multiply(numPairArr);
+  else if(operatorUsed == "÷") return divide(numPairArr);
+  else if(operatorUsed == "+") return add(numPairArr);
+  else if(operatorUsed == "–") return subtract(numPairArr);
+}
+
+function extractInputNumPair() {
+  return calcInput.value
+  .replaceAll("×", ",")
+  .replaceAll("÷", ",")
+  .replaceAll("+", ",")
+  .replaceAll("–", ",")
+  .split(",")
+  .map(num => Number(num));
+}
+
+function extractUsedOperator() {
+  const arithOperatorsValid = ["×", "÷", "+", "–"]
+  let operatorUsed = "";
+
+  for(const arithOperator of arithOperatorsValid) {
+    if(calcInput.value.indexOf(arithOperator) != -1) {
+      return calcInput.value[calcInput.value.indexOf(arithOperator)];
+    }
+  }
+}
+// Use const incase the function is ever attempted for some reason to be reassigned    
+const multiply = (numPairArr) => numPairArr[0] * numPairArr[1];
+const divide = (numPairArr) => numPairArr[0] / numPairArr[1];
+const add = (numPairArr) => numPairArr[0] + numPairArr[1];
+const subtract = (numPairArr) => numPairArr[0] - numPairArr[1];
 
